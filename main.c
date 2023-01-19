5e5daacc8378111005971b9c6721e0757b691b47
Huz <35820756+HuzPro@users.noreply.github.com>
2023-01-19 21:54:59 +0500

    added movement
    basic movement from tutorial
    also changed library include statements

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<SDL2/SDL.h>
-#include<SDL2/SDL_image.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
 #include "structs.h"
 #include "defs.h"
 #include "draw.c"
@@ -16,17 +16,34 @@ int main(int argc, char *argv[]) {
     memset(&snake, 0, sizeof(Snake));
     initSDL();
 
-    snake.x = 100;
-    snake.y = 100;
+    snake.x = 200;
+    snake.y = 200;
     snake.texture = loadTexture("images/snake/head-large.png");
 
     atexit(cleanup);
 
     while(1) {
         prepareScene();
+
         input();
+
+        if(app.up){
+            snake.y -= 3;
+        }
+        if(app.down){
+            snake.y += 3;
+        }
+        if(app.left){
+            snake.x -= 3;
+        }
+        if(app.right){
+            snake.x += 3;
+        }
+
         blit(snake.texture, snake.x, snake.y);
+
         presentScene();
+
         SDL_Delay(1000 / FPS);
     }
 }
