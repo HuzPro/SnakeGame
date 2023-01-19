b524fc215c27efb9c35bbab7851d253b72e84e4b
Huz <35820756+HuzPro@users.noreply.github.com>
2023-01-19 21:57:20 +0500

    added movement
    added 2 functions: doKeyUp and doKeyDown.
    also added checks for both events

--- input.c
+++ input.c
@@ -1,3 +1,55 @@
+void doKeyDown(SDL_KeyboardEvent *event)
+{
+	if (event->repeat == 0)
+	{
+		if (event->keysym.scancode == SDL_SCANCODE_UP)
+		{
+			app.up = 1;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_DOWN)
+		{
+			app.down = 1;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_LEFT)
+		{
+			app.left = 1;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_RIGHT)
+		{
+			app.right = 1;
+		}
+	}
+}
+
+void doKeyUp(SDL_KeyboardEvent *event)
+{
+	if (event->repeat == 0)
+	{
+		if (event->keysym.scancode == SDL_SCANCODE_UP)
+		{
+			app.up = 0;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_DOWN)
+		{
+			app.down = 0;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_LEFT)
+		{
+			app.left = 0;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_RIGHT)
+		{
+			app.right = 0;
+		}
+	}
+}
+
 void input() {
     SDL_Event event;
 
@@ -7,6 +59,16 @@ void input() {
             case SDL_QUIT:
                 exit(0);
                 break;
+
+            case SDL_KEYDOWN:
+				doKeyDown(&event.key);
+				break;
+
+			case SDL_KEYUP:
+				doKeyUp(&event.key);
+				break;
+
+
             default:
                 break;
         }
