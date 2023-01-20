d25391673c0051c3133d040f233c1e22e865e3bd
Huz <35820756+HuzPro@users.noreply.github.com>
2023-01-20 20:56:28 +0500

    Update movement.c

--- movement.c
+++ movement.c
@@ -1,41 +1,45 @@
 void movement(){
-	if(app.up && !app.down && !app.left && !app.right){
+	if(app.up && !app.down && !app.left && !app.right){			//true when only up key is being pressed
             if (snake.direction != 2){
             	snake.direction = 1;
-            	snake.angle = 180;
+            	snake.angle = 180;			//makes it look up
 			}
         }
-        if(app.down && !app.left && !app.right && !app.up){
+        if(app.down && !app.left && !app.right && !app.up){		//true when only down key is being pressed
             if(snake.direction != 1){
             	snake.direction = 2;
-            	snake.angle = 0;
+            	snake.angle = 0;			//makes it look down
 			}
         }
-        if(app.left && !app.right && !app.up && !app.down){
+        if(app.left && !app.right && !app.up && !app.down){		//true when only left key is being pressed
             if(snake.direction != 4){
             	snake.direction = 3;
-            	snake.angle = 90;
+            	snake.angle = 90;			//makes it look left
 			}
         }
-        if(app.right && !app.left && !app.up && !app.down){
+        if(app.right && !app.left && !app.up && !app.down){		//true when only right key is being pressed
             if(snake.direction != 3){
             	snake.direction = 4;
-            	snake.angle = 270;
+            	snake.angle = 270;			//makes it look right
 			}
         }
         
         switch(snake.direction){
-        	case 1:
-        		snake.y -= snake.speed;
+        	case 1:				//Moving up
+				if(snake.y > 2)
+        			snake.y -= snake.speed;
         		break;
-        	case 2:
-        		snake.y += snake.speed;
+        	case 2:				//Moving down
+				if(snake.y < SCREEN_HEIGHT-43)
+        			snake.y += snake.speed;
         		break;
-        	case 3:
-        		snake.x -= snake.speed;
+        	case 3:				//Moving left
+				if(snake.x > 2)
+        			snake.x -= snake.speed;
         		break;
-        	case 4:
-        		snake.x += snake.speed;
+        	case 4:				//Moving right
+				if(snake.x < SCREEN_WIDTH-43)
+    	    		snake.x += snake.speed;
         		break;
         	default:
         		break;
