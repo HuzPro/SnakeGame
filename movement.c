74abd5769e6450355c8ad3d015f45cacef30d046
Huz <35820756+HuzPro@users.noreply.github.com>
2023-01-20 12:30:16 +0500

    Added rotation

--- movement.c
+++ movement.c
@@ -1,15 +1,27 @@
 void movement(){
 	if(app.up && !app.down && !app.left && !app.right){
-            if (snake.direction != 2) snake.direction = 1;
+            if (snake.direction != 2){
+            	snake.direction = 1;
+            	snake.angle = 180;
+			}
         }
         if(app.down && !app.left && !app.right && !app.up){
-            if(snake.direction != 1)snake.direction = 2;
+            if(snake.direction != 1){
+            	snake.direction = 2;
+            	snake.angle = 0;
+			}
         }
         if(app.left && !app.right && !app.up && !app.down){
-            if(snake.direction != 4)snake.direction = 3;
+            if(snake.direction != 4){
+            	snake.direction = 3;
+            	snake.angle = 90;
+			}
         }
         if(app.right && !app.left && !app.up && !app.down){
-            if(snake.direction != 3)snake.direction = 4;
+            if(snake.direction != 3){
+            	snake.direction = 4;
+            	snake.angle = 270;
+			}
         }
         
         switch(snake.direction){
