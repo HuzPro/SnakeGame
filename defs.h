4baaad1b86c5c66ab1fbc5eddf663f37fbb1b372
Huz <35820756+HuzPro@users.noreply.github.com>
2023-01-20 20:56:04 +0500

    Update defs.h

--- defs.h
+++ defs.h
@@ -1,4 +1,4 @@
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
+int SCREEN_WIDTH = 1280; //changed from def to int cuz could be used to help with maximizing window for better adaptibility cross platform and cross device
+int SCREEN_HEIGHT = 720;
 #define FPS 60
 //#define speed 300 [isn't being used anywhere]
