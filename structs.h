99dbac480f55af2fe7101a81939053e4415a8ac5
Huz <35820756+HuzPro@users.noreply.github.com>
2023-01-31 15:16:09 +0500

    Update structs.h

--- structs.h
+++ structs.h
@@ -1,22 +1,54 @@
 typedef struct {
     SDL_Renderer *renderer;
     SDL_Window *window;
-
-    int up;
-    int down;
-    int left;
-    int right;
-
 } App;
 
 typedef struct {
-	int direction; //1 = up ^, 2 = down v, 3 = left <, 4 = right >
     int x;
     int y;
-    int speed;
-    int angle;
+} mousePos;
+
+typedef struct Snake{
+    int x;
+    int y;
+    int w;
+    int h;
+    double angle;
+    int dx;
+    int dy;
+    int turn;
     SDL_Texture *texture;
+    struct Snake *next;
+    struct Snake *prev;
 } Snake;
 
+typedef struct {
+    int x;
+    int y;
+    int w;
+    int h;
+    SDL_Texture *texture;
+} Apple;
+
+typedef struct {
+    SDL_Texture *head;
+    SDL_Texture *body;
+    SDL_Texture *tail;
+    SDL_Texture *turn;
+    SDL_Texture *apple;
+} Textures;
+
+typedef struct {
+	SDL_Rect title;
+    SDL_Rect play;
+    SDL_Rect score;
+    SDL_Rect quit;
+} Buttons;
+
+mousePos mouse;
 App app;
-Snake snake;
+Textures textures;
+Apple *apple = NULL;
+Snake *snake = NULL;
+Snake *turns = NULL;
+Buttons buttons;
